7a6d8c0fb676ccedf0553d062c810961424ea0a6
Albin B Thomas <albinbthomas8800@gmail.com>
2025-12-11 23:19:21 +0530

    Feat: Service Section

--- conponents/About.jsx
+++ conponents/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
      <div className="mt-10 flex flex-col px-4 md:px-20 md:mt-20">
 
-  {/* Section Heading */}
+  
   <div className="flex items-center justify-center gap-4 md:gap-6">
     <hr className="grow border-[#5D4634]" />
     <h1 className="text-center text-[16px] md:text-[24px] font-bold text-[#5D4634] whitespace-nowrap">
