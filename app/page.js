105bfc5ee015358fdf98caed656607756e4f86fd
Albin B Thomas <albinbthomas8800@gmail.com>
2025-12-16 20:08:29 +0530

    feat: cta

--- app/page.js
+++ app/page.js
@@ -15,6 +15,28 @@ export default function Home() {
       <Projects />
       <WhyUs />
       <Testimonials />
+
+      <div className="flex justify-center md:mt-20 mt-10">
+        <button
+          className="flex items-center gap-2 border text-[#816549] text-[15px]
+          px-6 py-3 rounded-lg
+          transition-all duration-300 hover:bg-[#816549] hover:text-white group"
+        >
+          Contact Our Team
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="w-5 h-5 group-hover:stroke-white"
+          >
+            <path d="M5 12h14M13 6l6 6-6 6" />
+          </svg>
+        </button>
+      </div>
     </>
   );
 }
