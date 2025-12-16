cf0517cab8ab73ea4a60602942c4557843158a2e
Albin B Thomas <albinbthomas8800@gmail.com>
2025-12-16 14:40:57 +0530

    feat: why choose us responsive

--- conponents/WhyUs.jsx
+++ conponents/WhyUs.jsx
@@ -11,31 +11,31 @@ export default function WhyUs() {
         <hr className="grow border-[#5D4634]" />
       </div>
 
-      <div className="grid grid-cols-4 mt-10 md:mt-20 gap-4 items-stretch">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-10 md:mt-20 gap-6 items-stretch">
         <div className="flex flex-col bg-white shadow-[0_0_15px_rgba(0,0,0,0.10)] rounded-2xl px-6 py-4 gap-4">
           <div className="flex items-center gap-2">
-            <img src="/icons/whyus1.svg" alt="" />
+            <img className="h-[26px] w-[26px]" src="/icons/whyus1.svg" alt="" />
             <h1 className="text-[18px] font-bold text-[#816549]">25+ Years of Experience</h1>
           </div>
           <p className="text-[14px] text-[#777777]">With 25 years of experience, we bring trusted expertise and dependable quality to every project.</p>
         </div>
         <div className="flex flex-col bg-white shadow-[0_0_15px_rgba(0,0,0,0.10)] rounded-2xl px-6 py-4 gap-4">
-          <div className="flex items-start gap-2">
-            <img src="/icons/whyus1.svg" alt="" />
+          <div className="flex items-star gap-2">
+            <img className="h-[26px] w-[26px]" src="/icons/whyus1.svg" alt="" />
             <h1 className="text-[18px] font-bold text-[#816549]">Quality Work & Durable Structures</h1>
           </div>
           <p className="text-[14px] text-[#777777]">With 25 years of experience, we bring trusted expertise and dependable quality to every project.</p>
         </div>
         <div className="flex flex-col bg-white shadow-[0_0_15px_rgba(0,0,0,0.10)] rounded-2xl px-6 py-4 gap-4">
           <div className="flex items-center gap-2">
-            <img src="/icons/whyus1.svg" alt="" />
+            <img className="h-[26px] w-[26px]" src="/icons/whyus1.svg" alt="" />
             <h1 className="text-[18px] font-bold text-[#816549]">25+ Years of Experience</h1>
           </div>
           <p className="text-[14px] text-[#777777]">With 25 years of experience, we bring trusted expertise and dependable quality to every project.</p>
         </div>
         <div className="flex flex-col bg-white shadow-[0_0_15px_rgba(0,0,0,0.10)] rounded-2xl px-6 py-4 gap-4">
           <div className="flex items-center gap-2">
-            <img src="/icons/whyus1.svg" alt="" />
+            <img className="h-[26px] w-[26px]" src="/icons/whyus1.svg" alt="" />
             <h1 className="text-[18px] font-bold text-[#816549]">25+ Years of Experience</h1>
           </div>
           <p className="text-[14px] text-[#777777]">With 25 years of experience, we bring trusted expertise and dependable quality to every project.</p>
